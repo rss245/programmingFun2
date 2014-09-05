3c61f97d8fd8d680ba5bb4bf3b841fa6e713d049
rss245 <rss245@gmail.com>
2014-09-05 16:04:36 -0400

    test 1 of my repository commit

---
+++ programmingFun2/src/com/befriendweb/client/FastClient.java
@@ -0,0 +1,12 @@
+package com.befriendweb.client;
+
+public class FastClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for( String g:args){
+			System.out.println("tagme:"+g);
+		}
+	}
+
+}
